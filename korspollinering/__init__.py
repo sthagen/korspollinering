39865f15546736411fdca65324f42b444d006bca
Stefan Hagen <stefan@hagen.link>
2023-04-19 22:04:48 +0200

    Bumped (empty) impl version

--- korspollinering/__init__.py
+++ korspollinering/__init__.py
@@ -1,8 +1,8 @@
 """Cross pollination (Swedish: korspollinering) of application data from editable files."""
 
 # [[[fill git_describe()]]]
-__version__ = '2023.4.20+parent.251ed4ec'
-# [[[end]]] (checksum: 7f4f1fc8ebb72f45d3f92f6a1088f480)
+__version__ = '2023.4.20+parent.61756ddb'
+# [[[end]]] (checksum: 0c1d3b246b02abf58c0523160a0ac08a)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
